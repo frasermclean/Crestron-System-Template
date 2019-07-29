b0d6a611264439524e03f6b8ae695bd50d9318f9
Fraser McLean <voltboyee@gmail.com>
2019-07-30 09:47:13 +1000

    Started system initialization implementation

--- Crestron-System-Template/ControlSystem.cs
+++ Crestron-System-Template/ControlSystem.cs
@@ -4,10 +4,58 @@ using Crestron.SimplSharpPro;
 using Crestron.SimplSharpPro.CrestronThread;
 using Crestron.SimplSharpPro.Diagnostics;
 using Crestron.SimplSharpPro.DeviceSupport;
+using Template.Interfaces;
 
 namespace Template
 {
-    public class ControlSystem : CrestronControlSystem
+    public class ControlSystem : CrestronControlSystem, ITrace
     {
+        #region System components
+        #endregion
+
+        #region Properties
+        public bool TraceEnabled { get; set; }
+        public string TraceName { get; set; }
+        #endregion
+
+        #region Initialization
+        public ControlSystem()
+            : base()
+        {
+            try
+            {
+                Thread.MaxNumberOfUserThreads = 20;
+            }
+            catch (Exception ex)
+            {
+                string message = String.Format("ControlSystem() exception caught: {0}", ex.Message);
+                Trace(message);
+                ErrorLog.Exception(message, ex);
+            }
+        }
+        public override void InitializeSystem()
+        {
+            try
+            {
+                // trace defaults
+                this.TraceEnabled = true;
+                this.TraceName = this.GetType().Name;
+            }
+            catch (Exception ex)
+            {
+                string message = String.Format("InitializeSystem() exception caught: {0}", ex.Message);
+                Trace(message);
+                ErrorLog.Exception(message, ex);
+            }
+        }
+        #endregion
+
+        #region Debugging
+        private void Trace(string message)
+        {
+            if (TraceEnabled)
+                CrestronConsole.PrintLine(String.Format("[{0}] {1}", TraceName, message.Trim()));
+        }
+        #endregion
     }
 }
